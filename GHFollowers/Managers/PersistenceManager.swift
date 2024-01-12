92951cac8d938aabae82d8c3ccde1ff3565285c7
Noalino <bgelineau.dev@gmail.com>
2024-01-12 15:16:48 +0100

    Save user in favorites

--- GHFollowers/Managers/PersistenceManager.swift
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -7,12 +7,40 @@
 
 import Foundation
 
+enum PersistenceActionType {
+    case add, remove
+}
+
 enum PersistenceManager {
 
     static private let defaults = UserDefaults.standard
 
     enum Keys {
         static let favorites = "favorites"
+    }
+
+    static func updateWith(favorite: Follower, actionType: PersistenceActionType, completed: @escaping (GFError?) -> Void) {
+        retrieveFavorites { result in
+            switch result {
+            case .success(var favorites):
+                switch actionType {
+                case .add:
+                    guard !favorites.contains(favorite) else {
+                        completed(.alreadyInFavorites)
+                        return
+                    }
+
+                    favorites.append(favorite)
+                case .remove:
+                    favorites.removeAll { $0.login == favorite.login }
+                }
+
+                completed(save(favorites: favorites))
+
+            case .failure(let error):
+                completed(error)
+            }
+        }
     }
 
     static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
--- GHFollowers/Utilities/GFError.swift
+++ GHFollowers/Utilities/GFError.swift
@@ -13,4 +13,5 @@ enum GFError: String, Error {
     case invalidResponse = "Invalid response from the server. Please try again."
     case invalidData = "The data received from the server was invalid. Please try again."
     case unableToFavorite = "There was an error favoriting this user. Please try again."
+    case alreadyInFavorites = "You've already favorites this user."
 }
