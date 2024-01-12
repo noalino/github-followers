63814aa6bf2ff6844715a33465b15651d700e093
Noalino <bgelineau.dev@gmail.com>
2024-01-12 14:59:37 +0100

    Setup persistence manager

---
+++ GHFollowers/Managers/PersistenceManager.swift
@@ -0,0 +1,43 @@
+//
+//  PersistenceManager.swift
+//  GHFollowers
+//
+//  Created by Noalino on 11/01/2024.
+//
+
+import Foundation
+
+enum PersistenceManager {
+
+    static private let defaults = UserDefaults.standard
+
+    enum Keys {
+        static let favorites = "favorites"
+    }
+
+    static func retrieveFavorites(completed: @escaping (Result<[Follower], GFError>) -> Void) {
+        guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else {
+            completed(.success([]))
+            return
+        }
+
+        do {
+            let decoder = JSONDecoder()
+            let favorites = try decoder.decode([Follower].self, from: favoritesData)
+            completed(.success(favorites))
+        } catch {
+            completed(.failure(.unableToFavorite))
+        }
+    }
+
+    static func save(favorites: [Follower]) -> GFError? {
+        do {
+            let encoder = JSONEncoder()
+            let encodedFavorites = try encoder.encode(favorites)
+            defaults.setValue(encodedFavorites, forKey: Keys.favorites)
+            return nil
+        } catch {
+            return .unableToFavorite
+        }
+    }
+}
