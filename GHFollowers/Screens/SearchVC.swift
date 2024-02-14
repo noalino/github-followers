115c0c4f8f6fb27c3c0e3f537b2d6e80867eb6c8
Noalino <bgelineau.dev@gmail.com>
2024-02-14 10:39:50 +0100

    Remove useless self

--- GHFollowers/Screens/SearchVC.swift
+++ GHFollowers/Screens/SearchVC.swift
@@ -31,7 +31,7 @@ class SearchVC: UIViewController {
     }
 
     func createDismissKeyboardTapGesture() {
-        let tap = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
+        let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
         view.addGestureRecognizer(tap)
     }
 
