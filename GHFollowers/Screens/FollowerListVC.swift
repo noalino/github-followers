3eb208f0afb5b60644f289af7b292d2986e1cb3b
Noalino <bgelineau.dev@gmail.com>
2024-02-16 09:49:44 +0100

    Fix scroll to top

--- GHFollowers/Screens/FollowerListVC.swift
+++ GHFollowers/Screens/FollowerListVC.swift
@@ -202,8 +202,7 @@ extension FollowerListVC: FollowerListVCDelegate {
         page = 1
         followers.removeAll()
         filteredFollowers.removeAll()
-        #warning("View is not scrolled at the very top")
-        collectionView.setContentOffset(.zero, animated: true)
+        collectionView.scrollToItem(at: IndexPath(item: 0, section: 0), at: .top, animated: true)
         getFollowers(username: username, page: page)
     }
 }
