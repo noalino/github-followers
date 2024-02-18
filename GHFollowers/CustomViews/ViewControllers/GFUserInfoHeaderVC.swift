62377d22c71371d8eba809e91ebb40e331e99b0e
Noalino <bgelineau.dev@gmail.com>
2024-02-18 10:40:13 +0100

    Refactor downloading avatar image

--- GHFollowers/CustomViews/ViewControllers/GFUserInfoHeaderVC.swift
+++ GHFollowers/CustomViews/ViewControllers/GFUserInfoHeaderVC.swift
@@ -34,17 +34,8 @@ class GFUserInfoHeaderVC: UIViewController {
         configureUIElements()
     }
 
-    func downloadAvatarImage() {
-        NetworkManager.shared.downloadImage(from: user.avatarUrl) { [weak self] image in
-            guard let self = self else { return }
-            DispatchQueue.main.async {
-                self.avatarImageView.image = image
-            }
-        }
-    }
-
     func configureUIElements() {
-        downloadAvatarImage()
+        avatarImageView.downloadImage(fromURL: user.avatarUrl)
         
         usernameLabel.text = user.login
         nameLabel.text = user.name ?? ""
